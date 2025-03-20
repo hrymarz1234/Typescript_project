05580a7962de452a454ae072b9e8b1acfac1af20
hrymarz1234 <126766484+hrymarz1234@users.noreply.github.com>
2025-03-20 21:05:22 +0100

    C

---
+++ ManagMe/src/App.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Project } from "./Objects";
+
+function App() {
+  
+  return (
+    <>
+      <h1>Create new project</h1>
+      <p>
+        <label>name</label>
+        <input type="text" id="name"></input>
+      </p>
+      <p>
+        <label>description</label>
+        <textarea id="description"></textarea>
+      </p>
+      <button onClick={CreateProject}>Add</button>
+    </>
+  );
+}
+let xpp = [] as Project[];
+
+if(localStorage.getItem("tab")!= null){
+  const xd = localStorage.getItem("tab")
+  xpp = JSON.parse(xd!);
+ }
+ 
+  
+  function CreateProject() {
+    console.log("byleco")
+    let proj: Project = {
+      id: Date.now(),
+      name: (document.querySelector("#name") as HTMLInputElement).value,
+      description: (document.querySelector("#description") as HTMLInputElement)
+        .value,
+    }
+    console.log(proj)
+    xpp.push(proj)
+    localStorage.setItem("tab", JSON.stringify(xpp));
+  }
+
+export default App;
